7d86b5832ec6fb87890008556fb7c649aff22aaa
jipeream <juan.perea.moraleda@gmail.com>
2015-12-03 17:06:01 +0100

    Committing

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,18 @@
 @echo off
 
 if not exist .\out\artifacts goto error
-if not exist .\deploy\bin goto error
+if not exist .\deploy goto error
 
-del .\deploy\fsinsights-*.*
+rem del .\deploy\bin\fsinsights-*.*
+
+mkdir .\deploy\bin
 xcopy /y .\out\artifacts\rss_input_endpoint_jar\*.* .\deploy\bin
-ren .\deploy\fsinsights-input-endpoints.jar rss-input-endpoint.jar
-xcopy /y .\out\artifacts\twitter_input_endpoint_jar\*.* .\deploy\
-ren .\deploy\fsinsights-input-endpoints.jar twitter-input-endpoint.jar
+ren .\deploy\bin\fsinsights-learning.jar fsn-rss-input-endpoint.jar
+xcopy /y .\out\artifacts\twitter_input_endpoint_jar\*.* .\deploy\bin
+ren .\deploy\bin\fsinsights-learning.jar fsn-twitter-input-endpoint.jar
+
+mkdir .\deploy\bin\config
+xcopy /y .\config\*.* .\deploy\bin\config
 
 goto exit
 
